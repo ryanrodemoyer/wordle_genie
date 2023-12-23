1d6e6f7e8c8b31b82e48f1722799782d87e77ee2
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2023-12-23 14:01:10 -0500

    clean up and first unit test

--- solver.test.js
+++ solver.test.js
@@ -1,22 +1,8 @@
-// const solver = require("./solver");
+const solver = require("./solver");
 const words = require("./words2");
 
-const wordIndex = (words) => {
-  const index = words.map((x) => {
-    return {
-      value: x,
-      letters: Array.from(x).reduce((acc, curr, j) => {
-        acc.push({ idx: j + 1, value: curr });
-        return acc;
-      }, []),
-    };
-  });
-
-  return index;
-};
-
 const config = {
-  words: wordIndex(words),
+  words: words,
   attempts: [
     {
       id: 1,
@@ -76,138 +62,13 @@ const config = {
   ],
 };
 
-const groupAtttempts = (attempts, i) => {
-  const attemptsCumulative = attempts.filter((x) => x.id <= i);
+test("five attempts, target word is slope", () => {
+  const result = solver(config);
 
-  const grouped = attemptsCumulative
-    .filter((x) => x.id <= i)
-    .flatMap((x) => x.letters)
-    .reduce((acc, curr) => {
-      if (acc.hasOwnProperty(curr.value)) {
-        if (acc[curr.value].find((x) => x.result !== curr.result)) {
-          acc[curr.value].push(curr);
-        }
-      } else {
-        acc[curr.value] = [curr];
-      }
-      return acc;
-    }, []);
-  return grouped;
-};
-
-const buildFilteredIndexes = (grouped, letter) => {
-  const filteredIndexes = [];
-  if (grouped[letter.value].length === 1) {
-    if (letter.result === "WARM") {
-      filteredIndexes.push(letter.idx);
-    } else {
-      filteredIndexes.push(...[1, 2, 3, 4, 5]);
-    }
-  } else {
-    filteredIndexes.push(
-      ...grouped[letter.value]
-        .filter((x) => x.result != "YES")
-        .map((x) => x.idx)
-    );
-  }
-  return filteredIndexes;
-};
-const processYes = (entry, letter) => {
-  for (let j = entry.possibilities.length - 1; j >= 0; j--) {
-    const check = entry.possibilities[j].letters.find(
-      (y) => y.idx === letter.idx && y.value === letter.value
-    );
-    if (!check) {
-      entry.possibilities.splice(j, 1);
-    }
-  }
-};
-
-const processWarm = (entry, letter, filteredIndexes) => {
-  for (let j = entry.possibilities.length - 1; j >= 0; j--) {
-    const check = entry.possibilities[j].letters
-      .filter((y) => filteredIndexes.includes(y.idx) === false)
-      .some((y) => y.value === letter.value);
-    if (!check) {
-      entry.possibilities.splice(j, 1);
-    }
-  }
-};
-
-const processNo = (entry, letter, filteredIndexes) => {
-  for (let j = entry.possibilities.length - 1; j >= 0; j--) {
-    const check = entry.possibilities[j].letters
-      .filter((y) => filteredIndexes.includes(y.idx))
-      .some((y) => y.value === letter.value);
-    if (check) {
-      entry.possibilities.splice(j, 1);
-    }
-  }
-};
-
-const solverPrivate = (words, attempts, i) => {
-  const entry = { idx: i, possibilities: [...words] };
-
-  const grouped = groupAtttempts(attempts, i);
-
-  for (const letter of attempts.flatMap((x) => x.letters)) {
-    const filteredIndexes = buildFilteredIndexes(grouped, letter);
-
-    switch (letter.result) {
-      case "YES":
-        processYes(entry, letter);
-        break;
-      case "WARM":
-        processWarm(entry, letter, filteredIndexes);
-        break;
-      case "NO":
-        processNo(entry, letter, filteredIndexes);
-        break;
-    }
-  }
-
-  return entry;
-};
-
-const solver = (config) => {
-  const results = Array.from({ length: config.attempts.length }, (_, i) =>
-    solverPrivate(
-      config.words,
-      config.attempts.filter((x) => x.id <= i + 1),
-      i + 1
-    )
-  );
-
-  return { attempts: results };
-};
-
-test("first test", () => {
-  const solverResult = solver(config);
-
-  // console.dir(JSON.stringify(solverResult));
-  console.dir(solverResult);
-});
-
-test("grouped", () => {
-  const arr = [1];
-  const arr2 = [3, 2];
-
-  arr.push(...arr2);
-
-  console.dir(arr);
-});
-
-test("wordIndex", () => {
-  const index = wordIndex(words);
-
-  //   console.dir(JSON.stringify(index));
-});
-
-test("explore", () => {
-  const no = config.attempts
-    .flatMap((x) => x.letters)
-    .filter((x) => x.result === "NO")
-    .map((x) => x.value);
-
-  //   console.dir(no);
+  expect(result.attempts.length).toBe(5);
+  expect(result.attempts[0].possibilities.length).toBe(4202);
+  expect(result.attempts[1].possibilities.length).toBe(43);
+  expect(result.attempts[2].possibilities.length).toBe(36);
+  expect(result.attempts[3].possibilities.length).toBe(3);
+  expect(result.attempts[4].possibilities.length).toBe(1);
 });
