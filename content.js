bb96987f861d8354c6799bf56637cc14df068e0a
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2023-12-25 15:33:26 -0500

    content refactor

--- content.js
+++ content.js
@@ -1,7 +1,151 @@
 // import words from "words";
 // import answers from "answers";
 
-var gameId = 0;
+const resultMap = {
+  present: "WARM",
+  absent: "NO",
+  correct: "YES",
+};
+
+const rowConfig = [
+  { id: 1, lbl: "Row 1", customId: "row-Row1" },
+  { id: 2, lbl: "Row 2", customId: "row-Row2" },
+  { id: 3, lbl: "Row 3", customId: "row-Row3" },
+  { id: 4, lbl: "Row 4", customId: "row-Row4" },
+  { id: 5, lbl: "Row 5", customId: "row-Row5" },
+  { id: 6, lbl: "Row 6", customId: "row-Row6" },
+];
+
+const processBoard = function () {
+  const rows = rowConfig.map((x) => {
+    const res = document.querySelector(`div[aria-label="${x.lbl}"]`);
+
+    const letters = Array.from(res.children).map((y, i) => {
+      const state = y.firstChild.getAttribute("data-state");
+      const value = y.firstChild.textContent.toUpperCase();
+      const result = resultMap[state];
+      return {
+        row: i + 1,
+        idx: x.id,
+        value,
+        result,
+      };
+    });
+
+    return {
+      id: x.id,
+      value: res.textContent,
+      letters: letters,
+    };
+  });
+
+  const attempts = rows.filter((x) => x.value);
+  const solverResult = solver({
+    words: window.words,
+    attempts: attempts,
+  });
+
+  rowConfig.forEach((x) => {
+    const r = document.getElementById(x.customId);
+    if (r) {
+      const a = solverResult.attempts.find((y) => y.idx === x.id);
+      if (a) {
+        if (a.possibilities.length <= 10) {
+          r.textContent = `${a.possibilities.length} - `;
+          r.textContent += a.possibilities.map((y) => y.value).join(",");
+        } else {
+          r.textContent = `${a.possibilities.length} remaining words`;
+        }
+      }
+    }
+  });
+
+  // solverResult.attempts.forEach((y) => {
+  //   const r = document.getElementById(`row-Row${y.idx}`);
+
+  // });
+
+  const module = document.querySelector("#wordle-app-game");
+  if (module) {
+    module.style.height = "650px";
+  }
+
+  const board = document.querySelector("[class*='Board-module_board__']");
+  if (board) {
+    board.style.height = "450px";
+  }
+};
+
+function gameInitialize(words) {
+  // one time setup
+  window.words = words;
+
+  // get all default rows from the wordle board
+  const rowsElement = document.querySelectorAll("[class*='Row-module_row']");
+  if (rowsElement) {
+    Array.from(rowsElement)
+      .map((x) => {
+        const lbl = x.getAttribute("aria-label").replace(" ", "");
+
+        const selectorName = `row-${lbl}`;
+
+        const div = document.createElement("div");
+        div.style.color = "white";
+        div.style.gridTemplateColumns = "1 / span 6";
+        div.id = selectorName;
+        div.textContent = " ";
+
+        const res = {
+          element: x,
+          col: div,
+        };
+        return res;
+      })
+      .forEach((x) => {
+        x.element.style.gridTemplateColumns = "repeat(6, 1fr)";
+        x.element.parentNode.insertBefore(x.col, x.element.nextSibling);
+      });
+
+    setInterval(processBoard, 1000);
+  }
+}
+
+function gameSearch() {
+  const game = document.getElementById("wordle-app-game");
+  if (game) {
+    fetch(chrome.runtime.getURL("/words.json"))
+      .then((response) => response.json())
+      .then((data) => gameInitialize(data))
+      .catch((error) => {
+        alert(
+          `error downloading word file. refresh the page to try again. message: ${error}`
+        );
+      })
+      .finally(() => clearInterval(intervalId_gameSearch));
+  }
+}
+
+var intervalId_gameSearch = setInterval(gameSearch, 500);
+
+function buttonSearch() {
+  const buttonIds = [
+    "button[data-testid='Continue']",
+    "button[data-testid='See Stats']",
+  ];
+
+  buttonIds.forEach((x) => {
+    const btn = document.querySelector(x);
+    if (btn) {
+      clearInterval(intervalId_buttonSearch);
+
+      btn.addEventListener("click", (elem) => {});
+
+      btn.click();
+    }
+  });
+}
+
+var intervalId_buttonSearch = setInterval(buttonSearch, 500);
 
 function adSearch() {
   const ids = ["ad-top"];
@@ -13,160 +157,4 @@ function adSearch() {
   });
 }
 
-const customRows = [];
-
-function gameInitialize(words) {
-  const rowsElement = document.querySelectorAll("[class*='Row-module_row']");
-  if (rowsElement) {
-    rowsElement.forEach((x) => {
-      x.style.gridTemplateColumns = "repeat(6, 1fr)";
-
-      const selectorName = `row${x
-        .getAttribute("aria-label")
-        .replace(" ", "")}`;
-      const div = document.createElement("div");
-      div.style.color = "white";
-      div.style.gridTemplateColumns = "1 / span 6";
-      div.id = selectorName;
-      div.textContent = " ";
-      x.parentNode.insertBefore(div, x.nextSibling);
-      customRows[selectorName] = div;
-    });
-  }
-
-  const customCols = [];
-
-  const callback = function () {
-    const rowIds = [
-      { id: 1, val: "Row 1" },
-      { id: 2, val: "Row 2" },
-      { id: 3, val: "Row 3" },
-      { id: 4, val: "Row 4" },
-      { id: 5, val: "Row 5" },
-      { id: 6, val: "Row 6" },
-    ];
-    const rows = rowIds.map((x) => {
-      const d = document.querySelector(`div[aria-label="${x.val}"]`);
-
-      const selectorName = `col${x.id}-custom`;
-      const customColsCollection = document.querySelector(`#${selectorName}`);
-      if (!customColsCollection) {
-        // todo redo this using a row below to show the extra info
-        // grid-template-columns: 1 / span 6
-        const div = document.createElement("div");
-        div.style.color = "white";
-        div.id = selectorName;
-        div.textContent = "";
-        d.appendChild(div);
-        customCols[x.id] = div;
-      }
-
-      const stateMap = {
-        present: "WARM",
-        absent: "NO",
-        correct: "YES",
-      };
-      const letters = Array.from(d.children)
-        .map((y, j) => {
-          let val = null;
-          try {
-            val = y.firstChild.getAttribute("data-state");
-          } catch (e) {
-            return null;
-          }
-
-          if (val === "tbd") {
-            throw new Error("wait longer");
-          }
-
-          return {
-            idx: j + 1,
-            value: y.firstChild.textContent.toUpperCase(),
-            result: stateMap[val],
-          };
-        })
-        .filter((y) => y != null);
-
-      return {
-        id: x.id,
-        value: d.textContent.toUpperCase(),
-        letters,
-      };
-    });
-
-    const solverResult = solver({
-      words,
-      attempts: rows.filter((x) => x.value),
-    });
-
-    solverResult.attempts.forEach((y) => {
-      const d = document.querySelector(`div[aria-label="Row ${y.idx}"]`);
-
-      const div = customCols[y.idx];
-      const divRow = customRows[`rowRow${y.idx}`];
-      div.textContent = ` `;
-      divRow.textContent = `${y.possibilities.length} remaining words`;
-      if (y.possibilities.length <= 10) {
-        divRow.textContent = `${y.possibilities.length} - `;
-
-        // div.textContent += y.possibilities.map((x) => x.value).join(",");
-        divRow.textContent += y.possibilities.map((x) => x.value).join(",");
-      }
-    });
-
-    const module = document.querySelector("#wordle-app-game");
-    if (module) {
-      module.style.height = "650px";
-    }
-
-    const board = document.querySelector("[class*='Board-module_board__']");
-    if (board) {
-      board.style.height = "450px";
-    }
-  };
-
-  setInterval(callback, 1000);
-
-  const row1divs = document.querySelectorAll('div[aria-label="Row 1"] > div');
-  if (row1divs) {
-  }
-}
-
-function gameSearch() {
-  const game = document.getElementById("wordle-app-game");
-  if (game) {
-    clearInterval(gameId);
-
-    fetch(chrome.runtime.getURL("/words.json"))
-      .then((response) => response.json())
-      .then((data) => gameInitialize(data))
-      .catch((error) => {
-        console.error("Error reading JSON file:", error);
-      });
-  }
-}
-
-function buttonSearch() {
-  const buttonIds = [
-    "button[data-testid='Continue']",
-    "button[data-testid='See Stats']",
-  ];
-
-  buttonIds.forEach((x) => {
-    const btn = document.querySelector(x);
-    if (btn) {
-      clearInterval(id);
-
-      btn.addEventListener("click", (elem) => {
-        adId = setInterval(adSearch, 500);
-        gameId = setInterval(gameSearch, 500);
-      });
-
-      btn.click();
-    } else {
-      // alert("not found");
-    }
-  });
-}
-
-var id = setInterval(buttonSearch, 500);
+var internalId_adSearch = setInterval(adSearch, 1000);
