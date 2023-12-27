1bc5c4e0c65da284d69a9773cd2c0399fd16f890
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2023-12-27 10:44:06 -0500

    file cleanup

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,6 @@
-// import { getIpInfo } from "./statics.js";
-
-chrome.runtime.onInstalled.addListener(function () {
+runtime.onInstalled.addListener(function () {
   console.log("Extension installed!");
   // Perform tasks on extension installation
-
-  //   await getIpInfo();
 });
 
 // Other background tasks
